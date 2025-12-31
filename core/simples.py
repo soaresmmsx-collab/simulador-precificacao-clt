1919b3231f902f92ddb88e7a90e6a7fd4cc02382
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 23:46:05 -0300

    Update simples.py

--- core/simples.py
+++ core/simples.py
@@ -15,3 +15,30 @@ def aliquota(rbt12, an):
     ultima = [f for f in TABELA_SIMPLES if f["anexo"] == an][-1]
     return (rbt12 * ultima["aliquota"] - ultima["deducao"]) / rbt12
 
+
+DAS_DETALHE = {
+    "III": {
+        "IRPJ": 0.04,
+        "CSLL": 0.035,
+        "PIS": 0.028,
+        "COFINS": 0.1274,
+        "CPP": 0.2885,
+        "ISS": 0.4711,
+    },
+    "V": {
+        "IRPJ": 0.055,
+        "CSLL": 0.035,
+        "PIS": 0.0305,
+        "COFINS": 0.141,
+        "CPP": 0.2785,
+        "ISS": 0.46,
+    }
+}
+
+def detalhar_das(valor_das, an):
+    return {
+        tributo: valor_das * perc
+        for tributo, perc in DAS_DETALHE[an].items()
+    }
+
+
