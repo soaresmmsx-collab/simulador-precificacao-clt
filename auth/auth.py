7ff4dde20aadbc649574ab6b5dfe95511c25c7c9
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 23:00:55 -0300

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -14,7 +14,7 @@ def login():
     p = st.text_input("Senha", type="password")
 
     if st.button("Entrar"):
-        if u in USERS and USERS[u] == _hash(p):
+        if u in USERS and USERS[p] == _hash(p):
             st.session_state.logged = True
             st.experimental_rerun()
         else:
