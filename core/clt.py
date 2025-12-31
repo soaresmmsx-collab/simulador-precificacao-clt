c6deb5be8e3cc73a0bb5135e87629b9fa359dcf6
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 22:42:16 -0300

    Update clt.py

--- core/clt.py
+++ core/clt.py
@@ -1,5 +1,21 @@
-ENCARGOS_CLT = 0.70  # fixo conforme planilha
+ENCARGOS = {
+    "INSS Patronal": 0.20,
+    "RAT": 0.01,
+    "FGTS": 0.08,
+    "FGTS adicional": 0.032,
+    "13º salário": 0.0833,
+    "Férias": 0.1111,
+    "1/3 constitucional": 0.037
+}
 
-def custo_unitario_clt(salario, beneficio):
-    encargos = salario * ENCARGOS_CLT
-    return salario + encargos + beneficio
+def calcular_clt(salario, beneficio):
+    detalhes = {}
+    total_encargos = 0
+
+    for nome, perc in ENCARGOS.items():
+        valor = salario * perc
+        detalhes[nome] = valor
+        total_encargos += valor
+
+    custo_total = salario + total_encargos + beneficio
+    return detalhes, custo_total
