40a9e72fe2e4406ae0602c06e7a6f41be4e63758
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 22:16:22 -0300

    Create auth.py

---
+++ auth/auth.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import hashlib
+
+USERS = {
+    "max": "681fae29b727e172cd8605c3444ec059c2340c9ae5941a9e9ecc46c0081db7c8"
+}
+
+def _hash(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def login():
+    st.title("🔐 Login")
+    user = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+        if user in USERS and USERS[user] == _hash(password):
+            st.session_state.logged = True
+            st.experimental_rerun()
+        else:
+            st.error("Usuário ou senha inválidos")
