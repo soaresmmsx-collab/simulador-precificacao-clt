3c7f6d39165eab915b70b51bd7c1c3bca84f4d55
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 23:07:37 -0300

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -2,20 +2,23 @@ import streamlit as st
 import hashlib
 
 USERS = {
-    "JTalent": "681fae29b727e172cd8605c3444ec059c2340c9ae5941a9e9ecc46c0081db7c8"
+    "max": "681fae29b727e172cd8605c3444ec059c2340c9ae5941a9e9ecc46c0081db7c8"
 }
 
-def _hash(p):
-    return hashlib.sha256(p.encode()).hexdigest()
+def _hash(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def login():
     st.title("🔐 Login")
-    u = st.text_input("Usuário")
-    p = st.text_input("Senha", type="password")
+
+    user = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
 
     if st.button("Entrar"):
-        if u in USERS and USERS[p] == _hash(p):
-            st.session_state.logged = True
-            st.experimental_rerun()
+        if user in USERS and USERS[user] == _hash(password):
+            st.session_state["logged"] = True
+            st.session_state["user"] = user
+            st.success("Login realizado com sucesso")
+            st.rerun()
         else:
             st.error("Usuário ou senha inválidos")
