76350c50a96a410c79977ee257606e1398697a2c
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 22:43:20 -0300

    Update simples.py

--- core/simples.py
+++ core/simples.py
@@ -1,13 +1,13 @@
 from data.tabelas_simples import TABELA_SIMPLES
 
-def fator_r(folha_anual, receita_anual):
-    return folha_anual / receita_anual if receita_anual > 0 else 0
+def fator_r(folha, receita):
+    return folha / receita if receita > 0 else 0
 
-def definir_anexo(fr):
+def anexo(fr):
     return "III" if fr >= 0.28 else "V"
 
-def aliquota_efetiva(rbt12, anexo):
+def aliquota(rbt12, an):
     for f in TABELA_SIMPLES:
-        if f["anexo"] == anexo and f["min"] <= rbt12 <= f["max"]:
-            return (rbt12 * f["aliquota"] - f["deducao"]) / rbt12
+        if f["anexo"] == an and f["min"] <= rbt12 <= f["max"]:
+            return (rbt12*f["aliquota"] - f["deducao"]) / rbt12
     return 0
