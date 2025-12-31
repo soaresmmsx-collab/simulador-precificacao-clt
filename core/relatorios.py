dce14f542f7c3a2dfdc1b2df0a096d6d3c376837
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 23:23:53 -0300

    Update relatorios.py

--- core/relatorios.py
+++ core/relatorios.py
@@ -1,6 +1,9 @@
 from jinja2 import Template
 from weasyprint import HTML
 
-def gerar_pdf(template_html, dados, nome):
-    html = Template(template_html).render(dados)
-    HTML(string=html).write_pdf(nome)
+def gerar_pdf(template_path, contexto, output_path):
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = Template(f.read())
+
+    html = template.render(contexto)
+    HTML(string=html).write_pdf(output_path)
